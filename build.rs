543c4a4857e393686b0dab8bf77ffb0d6180ce98
nomaterials <nomaterials@gmail.com>
2025-06-12 12:51:41 +0200

    skip package search if building docs for docs.rs

--- build.rs
+++ build.rs
@@ -4,6 +4,11 @@ fn main() {
 
 /// Use pkg-config to find system liblo
 fn try_pkg_config() {
+    // Skip lib search if building docs for docs.rs
+    if std::env::var("DOCS_RS").is_ok() {
+        return;
+    }
+
     // Probe system liblo via pkg-config
     let _ = pkg_config::Config::new()
         .atleast_version("0.32")
